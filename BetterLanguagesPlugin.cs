34f6035c50f177120ae690f79039c3e82feb6fbd
Yann Le Coroller <inXS212@gmail.com>
2021-03-13 14:32:41 +0100

    Quick Fixes
    Added References and gitignore

--- BetterLanguagesPlugin.cs
+++ BetterLanguagesPlugin.cs
@@ -12,19 +12,16 @@ namespace BetterLanguages
     public class BetterLanguagesPlugin : BasePlugin
     {
         public const string Id = "com.inxs212.betterlanguages";
-
         public ConfigEntry<string> LanguagesPath { get; private set; }
         public ConfigEntry<string> FallBackLanguage { get; private set; }
         public Harmony Harmony { get; } = new Harmony(Id);
 
         public static ManualLogSource log;
-
         public override void Load()
         {
             log = base.Log;
             LanguagesPath = Config.Bind("Paths", "Localization Directory", "Localizations");
             FallBackLanguage = Config.Bind("Language", "Default Language", "English");
-
 
             Harmony.PatchAll();
         }
